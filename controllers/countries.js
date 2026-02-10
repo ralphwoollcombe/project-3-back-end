33e0d45e13167d68e3f9055fbd7dee55ca02246b
ralphwoollcombe <ralph@ralphwoollcombe.com>
2026-02-10 16:35:28 +0000

    merge conflicts fixed

--- controllers/countries.js
+++ controllers/countries.js
@@ -1,23 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const verifyToken = require('../middleware/verify-token');
-const Country = require('../models/country');
-
-
-router.get('/', verifyToken, async (req, res) => {
-    try {
-        const countries = await Country.find({});
-        res.json(countries);
-    } catch (err) {
-        res.status(500).json({ err: err.message });
-    }
-});
-
-
-
-
-module.exports = router;
 const Quest = require('../models/quest');
 const Country = require('../models/country')
 const verifyToken = require('../middleware/verify-token');
