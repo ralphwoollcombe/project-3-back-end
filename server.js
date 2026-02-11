96dc106c776d5ed773485824aab465fb173ac574
ralphwoollcombe <ralph@ralphwoollcombe.com>
2026-02-11 12:53:30 +0000

    version wednesday 12.30PM

--- server.js
+++ server.js
@@ -24,11 +24,6 @@ app.use('/auth', authRouter)
 app.use('/users', usersRouter)
 app.use('/countries', countriesRouter)
 app.use('/users/:userId/quests', questsRouter)
-app.use('/countries', countriesRouter);
-
-
-
-
 
 const PORT = 3000
 app.listen (PORT,() => {
