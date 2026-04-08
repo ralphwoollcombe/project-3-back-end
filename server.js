1f02b781832637bb301ad0756baa95ae03960eba
Adil Khan <adil_97@hotmail.it>
2026-04-08 17:45:18 +0100

    fix render server config

--- server.js
+++ server.js
@@ -1,32 +1,45 @@
 const dotenv = require('dotenv');
 dotenv.config();
+
 const express = require('express');
 const app = express();
+
 const mongoose = require('mongoose');
 const cors = require('cors');
 const logger = require('morgan');
-const authRouter = require('./controllers/auth')
-const usersRouter = require ('./controllers/users')
-const questsRouter = require('./controllers/quests')
-const countriesRouter = require('./controllers/countries')
+
+// Routes
+const authRouter = require('./controllers/auth');
+const usersRouter = require('./controllers/users');
+const questsRouter = require('./controllers/quests');
+const countriesRouter = require('./controllers/countries');
+
+// Seed
 const seedCountries = require('./seed/seedCountries');
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI);
+
 mongoose.connection.on('connected', async () => {
-    console.log(`Connected to MongoDB ${mongoose.connection.name}`)
-    await seedCountries();
-})
+  console.log(`Connected to MongoDB ${mongoose.connection.name}`);
+  await seedCountries();
+});
 
-app.use(express.json())
-app.use(cors())
-app.use(logger('dev'))
-app.use('/auth', authRouter)
-app.use('/users', usersRouter)
-app.use('/countries', countriesRouter)
-app.use('/users/:userId/quests', questsRouter)
+app.use(express.json());
 
+app.use(cors({
+  origin: 'https://joyful-ganache-ae62a5.netlify.app',
+  credentials: true
+}));
 
-const PORT = process.env.PORT || 3000
-app.listen (PORT,() => {
-    console.log(`Server running on ${PORT} `)
-})
+app.use(logger('dev'));
+
+app.use('/auth', authRouter);
+app.use('/users', usersRouter);
+app.use('/countries', countriesRouter);
+app.use('/users/:userId/quests', questsRouter);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
